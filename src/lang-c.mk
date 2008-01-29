eb99fe21e9604de6143316231dd716a7b9a45b94
tromey@peakpeak.com <tromey@peakpeak.com@08d00423-9944-0410-8ac6-8fb78f471fac>
2008-01-29 15:12:16 +0000

    * src/lang-cxx.mk: New file. 	* src/lang-c.mk (%.o): Remove debug statement.  Add -c.

--- src/lang-c.mk
+++ src/lang-c.mk
@@ -3,10 +3,10 @@
 # FIXME: other depmodes
 # FIXME: static patterns and renaming?
 # FIXME: configury for CC
+# FIXME: fails when $(CC) has spaces -- need make functionality??
 # FIXME: needs secondary expansion
 %.o: %.c | .quagmire/$(CC)-compiler $(DEPDIR)
-	$(COMPILE.c) -MT $@ -MMD -MP -MF $(DEPDIR)/$*.Po $(OUTPUT_OPTION) $<
-	@echo got $(quagmire/depmode-$(CC))
+	$(COMPILE.c) -MT $@ -MMD -MP -MF $(DEPDIR)/$*.Po -c $(OUTPUT_OPTION) $<
 
 CC ?= gcc
 CFLAGS ?= -g -O2
