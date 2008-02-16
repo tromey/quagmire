2872cae283ca8dee332e353030aacbc596de3465
tromey@peakpeak.com <tromey@peakpeak.com@08d00423-9944-0410-8ac6-8fb78f471fac>
2008-02-16 20:05:23 +0000

    * src/compile.mk: Include defcompiler.mk.  Call 	quagmire/defcompiler for CXX, if needed. 	* src/post.mk: Don't include defcompiler.mk. 	* src/lang-cxx.mk (quagmire/cxx-rules): Changed arguments. 	Implement depmodes. 	* src/lang-c.mk (%.o): Don't use OUTPUT_OPTION in gcc3 case. 	* src/defcompiler.mk (quagmire/defcompiler): Only expand once per 	compiler.

--- src/defcompiler.mk
+++ src/defcompiler.mk
@@ -10,10 +10,16 @@ quagmire/compiler-name = $(subst /,@_,$(subst @,@@,$(firstword $(1))))
 # CC is the compiler.
 # NAME is the result of calling quagmire/compiler-name on the compiler.
 define quagmire/defcompiler
+
+# Only do this once per compiler.
+ifndef quagmire/depmode-$(1)
+
 .quagmire/$(1)-compiler: | .quagmire
 	@$(quagmire_dir)/check-depcomp $(quagmire_dir)/depcomp \
 		"quagmire/depmode-$(1)" "$(2)" > $$@
 
 -include .quagmire/$(1)-compiler
+
+endif
 
 endef
--- src/lang-c.mk
+++ src/lang-c.mk
@@ -9,7 +9,7 @@
 define quagmire/lang-c-rule
 %.o: %.c | $(DEPDIR)
 ifeq ($(quagmire/depmode-$(1)),gcc3)
-	$$(COMPILE.c) -MT $$@ -MMD -MP -MF $$(DEPDIR)/$*.Po -c $$(OUTPUT_OPTION) $$<
+	$$(COMPILE.c) -MT $$@ -MMD -MP -MF $$(DEPDIR)/$*.Po -c -o $$@ $$<
 else
 	depmode=$(quagmire/depmode-$(1)) source="$$<" object="$$@" \
 	DEPDIR="$$(DEPDIR)" libtool=no \
