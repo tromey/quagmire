276158c12727acfe14a120ba53b6c88c9f7ba67a
tromey@peakpeak.com <tromey@peakpeak.com@08d00423-9944-0410-8ac6-8fb78f471fac>
2008-02-16 22:53:56 +0000

    * src/post.mk: Install data, scripts. 	* src/data.mk (install/data-dir-$(1)): Depend on data, install 	dir. 	(quagmire/all-install-dirs): Update. 	(quagmire/data-directory): Add arguments.

--- src/data.mk
+++ src/data.mk
@@ -1,26 +1,37 @@
 # data.mk - Install data files.
 
-# Arg: dir-var
+# quagmire/data-directory DIR-VAR EXEC-OR-DATA PRIMARY
+# Install some files.
+# DIR-VAR is the name of the variable holding the directory,
+#   e.g., "datadir".
+# EXEC-OR-DATA is "exec" for install-exec, "data" for install-data.
+# PRIMARY is the automake "primary", usually DATA or SCRIPTS
 define quagmire/data-directory
 
-all: $$($(1)_DATA)
+all: $$($(1)_$(3))
 
-install/data-dir-$(1):
-	@list='$$($(1)_DATA)'; \
+# FIXME: the echo is old-style ... need to deal with the verbosity
+# thing.  Unlike automake we don't use a leading space, since that
+# looks weird with the other for loops.
+install/$(2)-dir-$(1): $$($(1)_$(3)) | $$(DESTDIR)$$($(1))
+	@list='$$($(1)_$(3))'; \
 	for f in $$$$list; do \
+	  $(if $(findstring s,$(MAKEFLAGS)),true,echo "$$(INSTALL) $$$$f $$(DESTDIR)$$($(1))/$$$$f"); \
 	  $$(INSTALL) $$$$f $$(DESTDIR)$$($(1))/$$$$f; \
 	done
-install-data: install/data-dir-$(1)
+install-$(2): install/$(2)-dir-$(1)
 
-uninstall/data-dir-$(1):
-	@list='$$($(1)_DATA)'; \
+# FIXME: the echo again
+uninstall/$(2)-dir-$(1):
+	@list='$$($(1)_$(3))'; \
 	for f in $$$$list; do \
+	  $(if $(findstring s,$(MAKEFLAGS)),true,echo "rm -f $$(DESTDIR)$$($(1))/$$$$f"); \
 	  rm -f $$(DESTDIR)$$($(1))/$$$$f; \
 	done
-uninstall: uninstall/data-dir-$(1)
+uninstall: uninstall/$(2)-dir-$(1)
 
-.PHONY: install/data-dir-$(1) uninstall/data-dir-$(1)
+.PHONY: install/$(2)-dir-$(1) uninstall/$(2)-dir-$(1)
+
+quagmire/all-install-dirs += $$($(1))
 
 endef
-
-$(eval $(foreach _dir,$(DIRECTORY_VARIABLES),$(call quagmire/data-directory,$(_dir)dir)))
