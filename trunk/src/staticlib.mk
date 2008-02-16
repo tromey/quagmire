b46fa44d475b661fdf74da62adb0388d54a2aea5
tromey@peakpeak.com <tromey@peakpeak.com@08d00423-9944-0410-8ac6-8fb78f471fac>
2008-02-16 06:36:58 +0000

    * src/install.mk: Fix comment. 	* src/staticlib.mk: Removed old-style comment. 	($(1)): Create library with correct name.

--- trunk/src/staticlib.mk
+++ trunk/src/staticlib.mk
@@ -1,30 +1,17 @@
 # staticlib.mk - Build a static library.
 
-# @deffn quagmire/library name
-# Define a static library named @var{name}.
-#
-# A library has a few attributes, each indicated by a variable whose
-# name is constructed from the library name.
-# @itemize @fixme
-# @item @var{name}_SOURCES
-# @item @var{name}_OBJECTS
-# @item @var{name}_INSTALLDIR
-# @item @var{name}_INSTALLNAME
-# @end itemize
-# extra vars
-#  archive = ar c
-# @end defun
+# quagmire/library NAME
+# Define a static library named NAME.
 define quagmire/library
 $(if $(1)_SOURCES,,$(error Library $(1) specified but $(1)_SOURCES not defined))
 
+# Note that we don't use ranlib.  It isn't needed on modern systems.
+# If you have a system that needs it, report a bug and we will fix it.
 $(call quagmire/aggregate,$(1))
-
-# FIXME: configure test for AR?
-# FIXME: ranlib, or do we care?
 
 # The rule to build the library.
 $(1): $$($(1)_OBJECTS)
-	$(ARCHIVE) lib$(1).a $$($(1)_OBJECTS)
+	$(ARCHIVE) $$@ $$($(1)_OBJECTS)
 
 endef
 
