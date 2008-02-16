fa8a5a6cc2df8da73bc91e2f6a89b71292941b95
tromey@peakpeak.com <tromey@peakpeak.com@08d00423-9944-0410-8ac6-8fb78f471fac>
2008-02-16 05:45:25 +0000

    * src/configuration.mk: Add distclean targets.

--- trunk/src/configuration.mk
+++ trunk/src/configuration.mk
@@ -8,7 +8,17 @@ define quagmire/config.status
 $(firstword $(1)): $(or $(wordlist 2,$(words $(1)),$(1)),$(1).in) config.status
 	./config.status $$@
 
+distclean/$(firstword $(1)):
+	rm -f $(firstword $(1))
+.PHONY: distclean/$(firstword $(1))
+distclean: distclean/$(firstword $(1))
+
 endef
 
 config.status: configure
 	./config.status --recheck
+
+distclean/config.status:
+	rm -rf config.status config.log autom4te.cache
+.PHONY: distclean/config.status
+distclean: distclean/config.status
