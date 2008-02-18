9a62c2bc880fd8fb740d84764be9f93f683253f7
tromey@peakpeak.com <tromey@peakpeak.com@08d00423-9944-0410-8ac6-8fb78f471fac>
2008-02-18 01:14:02 +0000

    * src/util.mk (quagmire/move-if-change): New function. 	* src/post.mk: Remove old comment. 	* src/configh.mk (quagmire/cfgh-header): New variable. 	(quagmire/cfgh-stamp): Likewise. 	($$(quagmire/cfgh-stamp)): New target. 	($(1)): Depend on stamp file.

--- trunk/src/util.mk
+++ trunk/src/util.mk
@@ -11,3 +11,7 @@ quagmire/space = $(quagmire/empty) $(quagmire/empty)
 # quagmire/value-name VALUE
 # Like tool-name but operates on arbitrary values, including lists.
 quagmire/value-name = $(subst $(quagmire/space),@s,$(subst /,@_,$(subst @,@@,$(strip $(1)))))
+
+# move-if-change FROM TO
+# Expand to a move-if-change shell script.
+quagmire/move-if-change = if test -r "$(1)" && cmp -s "$(1)" "$(2)"; then rm -f "$(1)"; else mv -f "$(1)" "$(2)"; fi
