f538b7ef184f7aee0842a4b53b16568a4f04430e
tromey@peakpeak.com <tromey@peakpeak.com@08d00423-9944-0410-8ac6-8fb78f471fac>
2008-02-18 17:37:44 +0000

    * src/checkheader.mk (.quagmire/header/%-frag): Handle a header	in 	a subdirectory. 	(.quagmire/header/%.c): Likewise.

--- trunk/src/checkheader.mk
+++ trunk/src/checkheader.mk
@@ -5,22 +5,23 @@
 
 .quagmire/header/%-frag: .quagmire/header/%.c
 ifeq (,$(findstring s,$(MAKEFLAGS)))
-	@echo -n "Checking for header $(*F)... "
+	@echo -n "Checking for header $*... "
 endif
-	@upper=`echo '$(*F)' | tr a-z. A-Z_`; \
-	if $(CC) $(CFLAGS) $(CPPFLAGS) $< -c -o .quagmire/header/$(*F).o > .quagmire/header/$(*F).log 2>&1; then \
+	@upper=`echo '$*' | tr a-z./ A-Z__`; \
+	if $(CC) $(CFLAGS) $(CPPFLAGS) $< -c -o .quagmire/header/$*.o > .quagmire/header/$*.log 2>&1; then \
 	  result="#define HAVE_$$upper 1"; msg=found; \
-	  rm .quagmire/header/$(*F).log; \
+	  rm .quagmire/header/$*.log; \
 	else \
 	  result="#undef HAVE_$$upper"; msg="not found"; \
 	fi; \
-	rm -f .quagmire/header/$(*F).o 2> /dev/null; \
+	rm -f .quagmire/header/$*.o 2> /dev/null; \
 	echo $$result > $@; \
 	$(if $(findstring s,$(MAKEFLAGS)),true,echo $$msg)
 
 .quagmire/header/%.c: | .quagmire/header
 	@rm -f $@.tmp
-	@echo "#include <$(*F)>" > $@.tmp
+	@mkdir -p $(@D)
+	@echo "#include <$*>" > $@.tmp
 	@echo "int x;" >> $@.tmp
 	@mv $@.tmp $@
 
