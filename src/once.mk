f2cda3cddc5f4515da522d57322d9815f11c97bf
tromey@peakpeak.com <tromey@peakpeak.com@08d00423-9944-0410-8ac6-8fb78f471fac>
2008-02-18 00:33:52 +0000

    comment fixes

--- src/once.mk
+++ src/once.mk
@@ -3,8 +3,6 @@
 # quagmire/expand-once KEY VALUE
 # If the expansion for KEY has already been done, return nothing.
 # Otherwise, return VALUE. Must be used in an eval context.
-# FIXME perhaps more efficient as a delayed function call
-# rather than pre-expanding the args?
 define quagmire/expand-once
 $(if $(quagmire/expanded-$(1)),,
 quagmire/expanded-$(1) := done
