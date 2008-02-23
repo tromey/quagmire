baa1352ce6a3d8ae66aab80f3c330ddf033ae4bf
tromey@peakpeak.com <tromey@peakpeak.com@08d00423-9944-0410-8ac6-8fb78f471fac>
2008-02-23 16:47:26 +0000

    * src/base.mk: Require GNU make 3.81.

--- src/base.mk
+++ src/base.mk
@@ -1,5 +1,9 @@
 # base.mk - the base Makefile.
 
+ifndef .FEATURES
+$(error Quagmire-based builds require GNU make 3.81 or later)
+endif
+
 VPATH = $(srcdir)
 
 # Ensure 'all' is the default target.
