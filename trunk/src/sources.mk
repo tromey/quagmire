3bdf9bf3568c2ecb3041dc8d0ab4c0837f5a27f9
tromey@peakpeak.com <tromey@peakpeak.com@08d00423-9944-0410-8ac6-8fb78f471fac>
2008-04-20 15:14:02 +0000

    * src/sources.mk (quagmire/source2obj): Replace all suffixes.

--- trunk/src/sources.mk
+++ trunk/src/sources.mk
@@ -5,10 +5,11 @@
 quagmire/filter-ignorable = $(filter-out %.h %.hh %.hxx %.H,$(1))
 
 # quagmire/source2obj SOURCE-LIST
-# Transform a list of sources to a list of object files.
-# The input list can contain any source files.
-# FIXME: how to handle user-provided extensions?
-# FIXME: handle OBJEXT?
-# Header files in the list are ignored.
+# Transform a list of sources to a list of object files.  The input
+# list can contain any source files.  Header files in the list are
+# ignored.  This simply removes the ignored files and then replaces
+# all remaining suffixes with the object extension.  This way, users
+# can easily extend the list of suffixes -- nothing special needs to
+# be done here.
 # FIXME: user-specified ignore list.
-quagmire/source2obj = $(patsubst %.c,%.$(OBJEXT),$(call quagmire/filter-ignorable,$(1)))
+quagmire/source2obj = $(addsuffix .$(OBJEXT),$(basename $(call quagmire/filter-ignorable,$(1))))
