ffa3499cf6fc8c649a90632dbb4801ff1f1641b4
tromey@peakpeak.com <tromey@peakpeak.com@08d00423-9944-0410-8ac6-8fb78f471fac>
2008-05-11 22:08:53 +0000

    ?      THANKS ?      examples M      src/staticlib.mk M      ChangeLog ?      example/simple/libzardoz.a ?      example/simple/Log ?      example/simple/generated.h ?      example/simple/cxxtest2 ?      example/simple/config.log ?      example/simple/config.status ?      example/simple/.quagmire ?      example/simple/QZ ?      example/simple/config.h ?      example/simple/ekeyring-1.5.1.tar.bz2 ?      example/simple/cxxtest ?      example/simple/cxxtest2.cc ?      example/simple/.deps ?      example/simple/quagmire ?      example/simple/ekeyring ?      example/simple/Makefile

--- src/staticlib.mk
+++ src/staticlib.mk
@@ -5,23 +5,28 @@
 define quagmire/library
 $(if $(or $($(1)_SOURCES),$(dist_$(1)_SOURCES),$(nodist_$(1)_SOURCES)),,$(error Library $(1) specified but no $(1)_SOURCES variable defined))
 
+# Prepare a list of convenience libraries that are merged into $(1).
+override quagmire/conv-libs = $$(filter-out /%, $$(filter %.a, $$($(1)_LIBS)))
+override quagmire/conv-lib-dirs = $$(patsubst %.a, %.ax, $$(notdir $$(quagmire/conv-libs)))
+override quagmire/conv-lib-files = $$(patsubst %.a, %.ax/*, $$(notdir $$(quagmire/conv-libs)))
+
 # Note that we don't use ranlib.  It isn't needed on modern systems.
 # If you have a system that needs it, report a bug and we will fix it.
 $(call quagmire/aggregate,$(1),$(2))
 
 # The rule to build the library.
-$(1): $$($(1)_OBJECTS) $$($(1)_LIBS)
-ifneq ($$($(1)_LIBS),)
-	@for i in $$($(1)_LIBS); do \
+$(1): $$($(1)_OBJECTS) $$(quagmire/conv-libs)
+ifneq ($$(quagmire/conv-libs),)
+	@for i in $$(quagmire/conv-libs); do \
 	  dir=`basename $$$${i}x` && \
 	  rm -rf $$$${dir} && \
 	  $(if $(findstring s,$(MAKEFLAGS)),true,echo "mkdir -p $$$${dir} && cd $$$${dir} && ar x ../$$$$i") && \
 	  mkdir -p $$$${dir} && cd $$$${dir} && ar x ../$$$$i; \
 	done
 endif
-	$(ARCHIVE) $$@ $$(patsubst %.a, %.ax/*, $$(notdir $$($(1)_LIBS))) $$($(1)_OBJECTS)
-ifneq ($$($(1)_LIBS),)
-	rm -rf $$(patsubst %.a, %.ax/*, $$(notdir $$($(1)_LIBS)))
+	$(ARCHIVE) $$@ $$(quagmire/conv-lib-files) $$($(1)_OBJECTS)
+ifneq ($(quagmire/conv-libs),)
+	rm -rf $$(quagmire-conv/lib-dirs)
 endif
 endef
 
